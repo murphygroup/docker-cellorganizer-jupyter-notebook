b6e42bb95f7e38504b3ef12c84265201c4a9cc11
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2019-01-17 17:45:56 -0500

    Updated build script

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ cd docker-python
 git checkout master
 cd ..
 
-wget -nc http://www.cellorganizer.org/Downloads/v2.7/docker/v2.7.2/cellorganizer-v2.7.2-models.tgz && \
-	tar -xvf cellorganizer-v2.7.2-models.tgz
-wget -nc http://www.cellorganizer.org/Downloads/v2.7/docker/v2.7.1/images/demo2D01.tgz && \
+wget -nc http://www.cellorganizer.org/Downloads/v2.8.0/docker/cellorganizer-v2.8.0-models.tgz && \
+	tar -xvf cellorganizer-v2.8.0-models.tgz
+wget -nc http://www.cellorganizer.org/Downloads/v2.8.0/docker/images/demo2D01.tgz && \
 	mkdir -p cellorganizer/images/HeLa/2D/LAMP2 && \
 	tar -xvf demo2D01.tgz -C cellorganizer/images/HeLa/2D/LAMP2/
 docker build -t $(whoami)/cellorganizer .
