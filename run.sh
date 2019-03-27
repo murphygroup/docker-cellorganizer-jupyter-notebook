0b491465dea416dbd8dba0e669fe73b5f3b26903
Ivan E. Cao-Berg <icaoberg@scs.cmu.edu>
2019-03-26 22:07:10 -0400

    Updated script

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 if [ ! -d data ]; then
 	mkdir data
+	wget -nc http://murphylab.web.cmu.edu/data/TcellModels/LATFull.tgz
+	tar -xvf LATFull.tgz
+	rm -fv LATFull.tgz
+
+	wget -nc http://murphylab.web.cmu.edu/data/Hela/3D/multitiff/cellorganizer_full_image_collection.zip
+	unzip cellorganizer_full_image_collection.zip
+	rm -fv cellorganizer_full_image_collection.zip
 fi
 
-docker run --rm -p 8888:8888 -v data:/home/murphylab/cellorganizer/data -e JUPYTER_LAB_ENABLE=yes murphylab/cellorganizer-jupyter
+docker run --rm -p 8888:8888 -v data:/home/murphylab/cellorganizer/local -e JUPYTER_LAB_ENABLE=yes murphylab/cellorganizer-jupyter
