270feee64549be94b1dae61ef706985cb55f412d
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2019-04-14 15:08:56 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ git submodule update
 cd cellorganizer-python
 git checkout master
 cd ..
-docker build --no-cache -t $(whoami)/cellorganizer-jupyter .
-
+docker build --no-cache -t murphylab/cellorganizer-jupyter:latest .
+docker tag murphylab/cellorganizer-jupyter:latest murphylab/cellorganizer-jupyter:v2.8.0
