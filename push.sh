3f8d5ad8ada7e3626ed6075cbb1fe808f13aec28
Ted Zhang <tedzhang@Teds-MacBook-Pro-2.local>
2023-02-28 17:32:38 -0500

    remove mmbios tag and make it universal. Also run.sh now points at dev container

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker push murphylab/cellorganizer-jupyter:latest
-docker push murphylab/cellorganizer-jupyter:v2.9.1
-docker push murphylab/cellorganizer-jupyter:mmbios2021
+docker push murphylab/cellorganizer-jupyter:v2.10.0
+docker push murphylab/cellorganizer-jupyter:dev
+# docker push murphylab/cellorganizer-jupyter:mmbios2021
--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ ! -d ~/Desktop/mmbios2021 ]; then
+if [ ! -d ~/Desktop/CellOrganizerDocker ]; then
         DIRECTORY=$(pwd)
       ####################################################################################################
 	    # icaoberg - this creates temporary folder on Desktop
 	    mkdir -p ~/Desktop/mmbios2021
-	    chmod a+rwx -R ~/Desktop/mmbios2021
-	    cd ~/Desktop/mmbios2021
+	    chmod a+rwx -R ~/Desktop/CellOrganizerDocker
+	    cd ~/Desktop/CellOrganizerDocker
       ####################################################################################################
 	cd $DIRECTORY
 fi
 
 ############################################################################################################
 docker run --rm -p 8888:8888 \
-	-v ~/Desktop/mmbios2021:/home/murphylab/cellorganizer/local \
+	-v ~/Desktop/CellOrganizerDocker:/home/murphylab/cellorganizer/local \
 	--memory="4g" \
 	--cpus=2 \
 	-e JUPYTER_LAB_ENABLE=yes \
-	murphylab/cellorganizer-jupyter
+	murphylab/cellorganizer-jupyter:dev
 ############################################################################################################
